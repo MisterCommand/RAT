adbe9280a232fa52e1846463a596397ad5c9de82
MisterCommand <anthonyhou04@gmail.com>
2022-02-28 21:33:54 +0800

    Add filter

--- src/App.svelte
+++ src/App.svelte
@@ -3,19 +3,27 @@
 	import Body from './Body.svelte';
 	import Table from './Table.svelte';
     import { Search, Checkbox, Row, Grid } from "carbon-components-svelte";
+
+    let search = "";
+    let doh = true;
+    let nmpa = true;
+    let fda = true;
+    let eu = true;
+
 </script>
 
 <Header class="header" />
 <Body>
-    <Search placeholder="搜索 Search" />
+    <Search placeholder="搜索 Search" bind:value={search} />
     <Grid>
         <Row>
-            <Checkbox labelText="Label text" checked />
-            <Checkbox labelText="Label text" checked />
-            <Checkbox labelText="Label text" checked />
+            <Checkbox labelText="衛生署 DOH" bind:checked={doh} />
+            <Checkbox labelText="藥監局 NMPA" bind:checked={nmpa} />
+            <Checkbox labelText="美國FDA" bind:checked={fda} />
+            <Checkbox labelText="歐盟 EU"  bind:checked={eu} />
         </Row>
     </Grid>
-    <Table />
+    <Table bind:search={search} bind:doh={doh} bind:nmpa={nmpa} bind:fda={fda} bind:eu={eu}/>
 </Body>
 
 <style>
--- src/Table.svelte
+++ src/Table.svelte
@@ -1,29 +1,64 @@
 <script>
     import { DataTable, Pagination } from "carbon-components-svelte";
-    
-  </script>
-  
-  <DataTable
+    import rat from './rat.csv';
+
+    let data = rat;
+    let page = 1;
+    export let search;
+    export let doh;
+    export let nmpa;
+    export let fda;
+    export let eu;
+
+    // Search and Filter
+    function filter() {
+        search = search.toLowerCase();
+        data = rat.filter((value) => {
+            return value.manufacturer.toLowerCase().match(`${search}.*`) || value.product.toLowerCase().match(`${search}.*`) || value.approval.toLowerCase().match(`${search}.*`)
+        })
+        let approved = [];
+        if (doh) {
+            approved.push("衛生署 DOH");
+        }
+        if (nmpa) {
+            approved.push("藥監局 NMPA");
+        }
+        if (fda) {
+            approved.push("美國FDA");
+        }
+        if (eu) {
+            approved.push("歐盟 EU");
+        }
+        data = data.filter((value) => {
+            // False -> If Any Match -> True
+            let output = false;
+            approved.forEach(v => {
+                if (value.approval == v) {
+                    output = true
+                }
+            });
+            return output;
+        })
+    }
+
+    $: filter(search, doh, nmpa, fda, eu)
+
+</script>
+<DataTable
     sortable
     headers={[
-      { key: "name", value: "Name" },
-      { key: "protocol", value: "Protocol" },
-      { key: "port", value: "Port" },
-      { key: "cost", value: "Cost", display: (cost) => cost + " €" },
-      {
-        key: "expireDate",
-        value: "Expire date",
-        display: (date) => new Date(date).toLocaleString(),
-        sort: (a, b) => new Date(a) - new Date(b),
-      },
+      { key: "manufacturer", value: "品牌 Brand" },
+      { key: "product", value: "產品 Product" },
+      { key: "approval", value: "認證機構 Approved by" }
     ]}
-    pageSize={10}
-    page={1}
-    rows={[]}
+    pageSize={15}
+    bind:page={page}
+    bind:rows={data}
   />
   <Pagination
-    pageSize={20}
-    totalItems={6}
+    pageSize={15}
+    bind:page={page}
+    bind:totalItems={data.length}
     pageSizeInputDisabled
     pageInputDisabled
   />
